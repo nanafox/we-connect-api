2bc4bd354865cad91328a82849b0493b32aaf9df
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-06-09 23:07:36 +0000

    refactor: Update FastAPI configuration and add API documentation
    This commit updates the FastAPI configuration in main.py to include additional metadata and information about the API. It sets the server URL to "http://localhost:8000", adds a title and version to the API documentation, and includes tags and descriptions for the endpoints related to posts and users. This change enhances the overall documentation and improves the clarity and usability of the API for developers.

--- posts_app/api/main.py
+++ posts_app/api/main.py
@@ -1,125 +1,37 @@
-# flake8: noqa: B008
-
 from dotenv import load_dotenv
-from fastapi import Depends, FastAPI, status
-from sqlalchemy.orm import Session
+from fastapi import FastAPI
 
-from posts_app import crud, models, schemas
-from posts_app.database import engine, get_db
+from posts_app import models, schemas
+from posts_app.api.routers import auth, posts, users
+from posts_app.database import engine
 
 load_dotenv()
 
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(
+    servers=[{"url": "http://localhost:8000"}],
+    title="Social Media App REST API",
+    version="0.1.0",
+    openapi_tags=[
+        {"name": "Posts", "description": "Endpoints related to posts."},
+        {"name": "Users", "description": "Endpoints related to users."},
+    ],
+    summary="This is a REST API for a social media app.",
+    description="This REST API allows users to create, read, update, and delete posts and users.",
+)
 
-crud_post = crud.APICrudBase[models.Post, schemas.Post](models.Post)
-
-crud_user = crud.APICrudBase[models.User, schemas.User](models.User)
+app.include_router(users.router)
+app.include_router(posts.router)
+app.include_router(auth.router)
 
 
 @app.get(
     "/status",
     response_description="API status OK",
     response_model=schemas.StatusResponse,
+    summary="API status",
 )
 def get_api_status():
     """This endpoint returns OK if the API server is up and running."""
     return {"status": "OK"}
-
-
-@app.get("/users", response_model=list[schemas.User])
-async def get_users(
-    skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
-):
-    return crud_user.get_all(db=db, skip=skip, limit=limit)
-
-
-@app.get("/users/{user_id}", response_model=schemas.User)
-async def get_user(user_id: str, db: Session = Depends(get_db)):
-    return crud_user.get_by_id(db=db, id=user_id)
-
-
-@app.put("/users/{user_id}", response_model=schemas.User)
-async def update_user(
-    user_id: str,
-    user: schemas.UserCreateUpdate,
-    db: Session = Depends(get_db),
-):
-    return crud_user.update(db=db, schema=user, id=user_id)
-
-
-@app.post(
-    "/users", status_code=status.HTTP_201_CREATED, response_model=schemas.User
-)
-async def create_user(
-    user: schemas.UserCreateUpdate, db: Session = Depends(get_db)
-):
-    return crud_user.create(db=db, schema=user)
-
-
-@app.delete("/users/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_user(user_id: str, db: Session = Depends(get_db)):
-    """This endpoint deletes a user by its id."""
-    return crud_user.delete(id=user_id, db=db)
-
-
-@app.get("/posts", response_model=schemas.PostsList)
-async def get_posts(
-    skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
-):
-    posts = crud_post.get_all(db=db, skip=skip, limit=limit)
-    metadata = schemas.MetaData(
-        links=schemas.Link(next=None, previous=None),
-        status_code=status.HTTP_200_OK,
-        count=len(posts),
-        total_pages=1,
-        current_page=1,
-    )
-    return {"data": posts, "metadata": metadata}
-
-
-@app.get("/users/{user_id}/posts")
-async def get_user_posts(user_id: str):
-    return {"message": f"Posts of user {user_id}"}
-
-
-@app.post(
-    "/posts",
-    status_code=status.HTTP_201_CREATED,
-    response_model=schemas.Post,
-)
-async def create_post(
-    post: schemas.PostCreateUpdate, db: Session = Depends(get_db)
-):
-    return crud_post.create(db=db, schema=post)
-
-
-@app.get("/posts/{post_id}", response_model=schemas.Post)
-async def get_post(post_id: str, db: Session = Depends(get_db)):
-    """This endpoint returns a single post by its id."""
-    return crud_post.get_by_id(db=db, id=post_id)
-
-
-@app.delete("/posts/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_post(post_id: str, db: Session = Depends(get_db)):
-    """This endpoint deletes a post by its id."""
-    return crud_post.delete(post_id=post_id, db=db)
-
-
-@app.put("/posts/{post_id}", response_model=schemas.Post)
-async def update_post(
-    post_id: str,
-    post: schemas.PostCreateUpdate,
-    db: Session = Depends(get_db),
-):
-    return crud_post.update(db=db, schema=post, id=post_id)
-
-
-@app.patch("/posts/{post_id}", response_model=schemas.Post)
-async def partial_update_post(
-    post_id: str,
-    post: schemas.PostPartialUpdate,
-    db: Session = Depends(get_db),
-):
-    return crud_post.partial_update(db=db, schema=post, id=post_id)
