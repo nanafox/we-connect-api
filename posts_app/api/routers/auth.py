81acbc6abee2e96e010240c34ae8632ecffec78d
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-06-10 23:17:59 +0000

    refactor: Add login endpoint for user authentication

--- posts_app/api/routers/auth.py
+++ posts_app/api/routers/auth.py
@@ -1,30 +1,48 @@
-# flake8: noqa: B008
-
+from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException, status
-from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordRequestForm
 
-from posts_app import crud, models, schemas
-from posts_app.database import get_db
+from posts_app import models, oauth2, schemas
+from posts_app.api.routers.deps import DBSessionDependency
 from posts_app.utils import is_valid_password
 
 router = APIRouter(tags=["Authentication"])
 
-crud_user = crud.APICrudBase[models.User, schemas.User](models.User)
 
+@router.post("/login", response_model=schemas.Token)
+def login(
+    user_credentials: Annotated[OAuth2PasswordRequestForm, Depends()],
+    db: DBSessionDependency,
+) -> schemas.Token:
+    user = (
+        db.query(models.User)
+        .filter_by(email=user_credentials.username)
+        .first()
+    )
 
-@router.post("/login")
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(get_db)):
-    user = crud_user.get_all(db=db, email=user_credentials.email)[0]
     if not user:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Invalid Credentials",
+            detail="Invalid credentials",
+            status_code=status.HTTP_400_BAD_REQUEST,
         )
 
-    if not is_valid_password(user_credentials.password, user.password):
+    if not is_valid_password(
+        plain_password=user_credentials.password,
+        hashed_password=user.password,
+    ):
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Invalid Credentials",
+            detail="Invalid credentials",
+            status_code=status.HTTP_400_BAD_REQUEST,
         )
-    return {"message": "Successfully logged in"}
+
+    access_token = oauth2.create_access_token(
+        data={"sub": str(user.id), "email": user.email}
+    )
+
+    # trunk-ignore(bandit/B106)
+    return schemas.Token(
+        access_token=access_token,
+        token_type="bearer",
+        expire_in=oauth2.ACCESS_TOKEN_EXPIRE_MINUTES,
+    )
