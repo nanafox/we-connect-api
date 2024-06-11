4620ded21ea3ee19ef659cc1c60ea5594b0afa59
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-06-11 00:02:28 +0000

    refactor: Update code to handle dependencies and perform general code clean up

--- posts_app/api/routers/posts.py
+++ posts_app/api/routers/posts.py
@@ -1,21 +1,17 @@
-# flake8: noqa: B008
+from fastapi import APIRouter, status
 
-from fastapi import APIRouter, Depends, status
-from sqlalchemy.orm import Session
-
-from posts_app import crud, models, schemas
-from posts_app.database import get_db
-from posts_app.utils import get_query_params
+from posts_app import schemas
+from posts_app.api.routers import CurrentUserDependency
+from posts_app.api.routers.deps import DBSessionDependency, QueryParamsDependency
+from posts_app.crud import crud_post
 
 router = APIRouter(prefix="/posts", tags=["Posts"])
-
-crud_post = crud.APICrudBase[models.Post, schemas.Post](models.Post)
 
 
 @router.get("/", response_model=schemas.PostsList)
 async def get_posts(
-    db: Session = Depends(get_db),
-    query_params: dict = Depends(get_query_params),
+    query_params: QueryParamsDependency,
+    db: DBSessionDependency,
 ):
     posts = crud_post.get_all(db=db, **query_params)
 
@@ -29,47 +25,52 @@ async def get_posts(
     return {"data": posts, "metadata": metadata}
 
 
-@router.get("/{user_id}/posts")
-async def get_user_posts(user_id: str):
-    return {"message": f"Posts of user {user_id}"}
-
-
 @router.post(
     "/",
     status_code=status.HTTP_201_CREATED,
     response_model=schemas.Post,
 )
 async def create_post(
-    post: schemas.PostCreateUpdate, db: Session = Depends(get_db)
-):
-    return crud_post.create(db=db, schema=post)
+    post: schemas.PostCreateUpdate,
+    db: DBSessionDependency,
+    user: CurrentUserDependency,
+) -> schemas.Post:
+    return crud_post.create(db=db, schema=post, obj_owner_id=user.id)
 
 
 @router.get("/{post_id}", response_model=schemas.Post)
-async def get_post(post_id: str, db: Session = Depends(get_db)):
+async def get_post(post_id: str, db: DBSessionDependency):
     """This endpoint returns a single post by its id."""
     return crud_post.get_by_id(db=db, id=post_id)
 
 
 @router.delete("/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_post(post_id: str, db: Session = Depends(get_db)):
+async def delete_post(
+    post_id: str, db: DBSessionDependency, user: CurrentUserDependency
+):
     """This endpoint deletes a post by its id."""
-    return crud_post.delete(post_id=post_id, db=db)
+    return crud_post.delete(id=post_id, db=db, obj_owner_id=user.id)
 
 
 @router.put("/{post_id}", response_model=schemas.Post)
 async def update_post(
     post_id: str,
     post: schemas.PostCreateUpdate,
-    db: Session = Depends(get_db),
+    db: DBSessionDependency,
+    user: CurrentUserDependency,
 ):
-    return crud_post.update(db=db, schema=post, id=post_id)
+    return crud_post.update(
+        db=db, schema=post, id=post_id, obj_owner_id=user.id
+    )
 
 
 @router.patch("/{post_id}", response_model=schemas.Post)
 async def partial_update_post(
     post_id: str,
     post: schemas.PostPartialUpdate,
-    db: Session = Depends(get_db),
+    db: DBSessionDependency,
+    user: CurrentUserDependency,
 ):
-    return crud_post.partial_update(db=db, schema=post, id=post_id)
+    return crud_post.partial_update(
+        db=db, schema=post, id=post_id, obj_owner_id=user.id
+    )
--- posts_app/api/routers/users.py
+++ posts_app/api/routers/users.py
@@ -1,15 +1,14 @@
-# flake8: noqa: B008
+from fastapi import APIRouter, HTTPException, status
 
-from fastapi import APIRouter, Depends, status
-from sqlalchemy.orm import Session
-
-from posts_app import crud, models, schemas
-from posts_app.database import get_db
-from posts_app.utils import get_query_params
+from posts_app import schemas
+from posts_app.api.routers import CurrentUserDependency
+from posts_app.api.routers.deps import (
+    DBSessionDependency,
+    QueryParamsDependency,
+)
+from posts_app.crud import crud_user
 
 router = APIRouter(prefix="/users", tags=["Users"])
-
-crud_user = crud.APICrudBase[models.User, schemas.User](models.User)
 
 
 @router.get(
@@ -18,11 +17,19 @@ crud_user = crud.APICrudBase[models.User, schemas.User](models.User)
     response_description="Users retrieved successfully",
 )
 async def get_users(
-    db: Session = Depends(get_db),
-    query_params: dict = Depends(get_query_params),
+    query_params: QueryParamsDependency,
+    db: DBSessionDependency,
 ):
     """Returns a list of users."""
     return crud_user.get_all(db=db, **query_params)
+
+
+@router.get("/me", response_model=schemas.User)
+async def get_current_user(
+    current_user: CurrentUserDependency,
+):
+    """This endpoint returns the current user."""
+    return current_user
 
 
 @router.get(
@@ -30,7 +37,7 @@ async def get_users(
     response_model=schemas.User,
     response_description="User retrieved successfully",
 )
-async def get_user(user_id: str, db: Session = Depends(get_db)):
+async def get_user(user_id: str, db: DBSessionDependency):
     return crud_user.get_by_id(db=db, id=user_id)
 
 
@@ -38,21 +45,38 @@ async def get_user(user_id: str, db: Session = Depends(get_db)):
 async def update_user(
     user_id: str,
     user: schemas.UserCreateUpdate,
-    db: Session = Depends(get_db),
+    db: DBSessionDependency,
+    current_user: CurrentUserDependency,
 ):
-    return crud_user.update(db=db, schema=user, id=user_id)
+    return crud_user.update(
+        db=db, schema=user, id=user_id, obj_owner_id=current_user.id
+    )
 
 
 @router.post(
     "/", status_code=status.HTTP_201_CREATED, response_model=schemas.User
 )
 async def create_user(
-    user: schemas.UserCreateUpdate, db: Session = Depends(get_db)
+    user: schemas.UserCreateUpdate,
+    db: DBSessionDependency,
+    current_user: CurrentUserDependency,
 ):
+    """This endpoint creates a new user."""
+    if current_user:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail={
+                "message": "This action is forbidden.",
+                "reason": "You own an account already.",
+            },
+        )
+
     return crud_user.create(db=db, schema=user)
 
 
 @router.delete("/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_user(user_id: str, db: Session = Depends(get_db)):
+async def delete_user(
+    user_id: str, db: DBSessionDependency, current_user: CurrentUserDependency
+):
     """This endpoint deletes a user by its id."""
-    return crud_user.delete(id=user_id, db=db)
+    return crud_user.delete(id=user_id, db=db, obj_owner_id=current_user.id)
