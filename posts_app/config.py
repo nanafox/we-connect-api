7ce76f2be6efd330c6685822744d51e2fde7ab8f
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-06-13 13:13:44 +0000

    refactor: Add API configuration and settings module

---
+++ posts_app/config.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    """Schema for API settings."""
+    db_host: str = "localhost"
+    db_user: str
+    db_password: str
+    db_name: str
+    db_port: int
+    secret_key: str
+    oauth2_algorithm: str
+    access_token_expire_minutes: int | datetime
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
