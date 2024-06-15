bc723c535f10bad6c917acea836d0431119ed8ae
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-06-15 10:59:00 +0000

    refactor: Update the models to use `mapped_column` of SQLAlchemy version 2

--- posts_app/models.py
+++ posts_app/models.py
@@ -2,16 +2,8 @@ from datetime import datetime
 from uuid import uuid4
 
 import sqlalchemy
-from sqlalchemy import (
-    TIMESTAMP,
-    Boolean,
-    Column,
-    ForeignKey,
-    String,
-    func,
-    text,
-)
-from sqlalchemy.orm import relationship
+from sqlalchemy import Boolean, ForeignKey, func, String, text, TIMESTAMP
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from posts_app.database import Base
 from posts_app.utils import UtilMixin
@@ -22,7 +14,7 @@ class Post(Base, UtilMixin):
 
     __tablename__ = "posts"
 
-    id = Column(
+    id: Mapped[sqlalchemy.Uuid] = mapped_column(
         sqlalchemy.Uuid,
         primary_key=True,
         default=uuid4,
@@ -30,24 +22,25 @@ class Post(Base, UtilMixin):
         unique=True,
         index=True,
     )
-    title = Column(String, index=True, nullable=False)
-    content = Column(String, nullable=False, index=True)
-    published = Column(Boolean, server_default="True", nullable=False)
-    created_at = Column(
-        TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
+    title: Mapped[str] = mapped_column(String, index=True)
+    content: Mapped[str] = mapped_column(String, index=True)
+    published: Mapped[bool] = mapped_column(
+        Boolean, server_default="True", nullable=False
     )
-    updated_at = Column(
+    created_at: Mapped[TIMESTAMP] = mapped_column(
+        TIMESTAMP(timezone=True), server_default=text("now()")
+    )
+    updated_at: Mapped[TIMESTAMP] = mapped_column(
         TIMESTAMP(timezone=True),
         nullable=False,
         server_default=text("now()"),
         onupdate=datetime.now,
     )
-    user_id = Column(
+    user_id: Mapped[sqlalchemy.Uuid] = mapped_column(
         sqlalchemy.Uuid,
         ForeignKey("users.id", ondelete="CASCADE"),
-        nullable=False,
     )
-    user = relationship("User")
+    user: Mapped["User"] = relationship()
 
     def __str__(self):
         return f"Post title: {self.title}"
@@ -56,7 +49,7 @@ class Post(Base, UtilMixin):
 class User(Base, UtilMixin):
     __tablename__ = "users"
 
-    id = Column(
+    id: Mapped[sqlalchemy.Uuid] = mapped_column(
         sqlalchemy.Uuid,
         primary_key=True,
         default=uuid4,
@@ -64,14 +57,13 @@ class User(Base, UtilMixin):
         unique=True,
         index=True,
     )
-    email = Column(String, nullable=False, unique=True, index=True)
-    password = Column(String, nullable=False)
-    created_at = Column(
-        TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
+    email: Mapped[str] = mapped_column(String, unique=True, index=True)
+    password: Mapped[str] = mapped_column(String)
+    created_at: Mapped[TIMESTAMP] = mapped_column(
+        TIMESTAMP(timezone=True), server_default=text("now()")
     )
-    updated_at = Column(
+    updated_at: Mapped[TIMESTAMP] = mapped_column(
         TIMESTAMP(timezone=True),
-        nullable=False,
         server_default=text("now()"),
         onupdate=datetime.now,
     )
@@ -83,12 +75,12 @@ class User(Base, UtilMixin):
 class Vote(Base, UtilMixin):
     __tablename__ = "votes"
 
-    user_id = Column(
+    user_id: Mapped[sqlalchemy.Uuid] = mapped_column(
         sqlalchemy.Uuid,
         ForeignKey("users.id", ondelete="CASCADE"),
         primary_key=True,
     )
-    post_id = Column(
+    post_id: Mapped[sqlalchemy.Uuid] = mapped_column(
         sqlalchemy.Uuid,
         ForeignKey("posts.id", ondelete="CASCADE"),
         primary_key=True,
